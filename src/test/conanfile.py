40b7b8d8a4771b46ddc21d70d87a2402f5874ba8
Oscar Perez <odperez@sodimac.cl>
2020-05-14 13:42:15 -0400

    Cambio de version 0.1.2 test

--- src/test/conanfile.py
+++ src/test/conanfile.py
@@ -3,17 +3,17 @@ from conans import tools
 
 class TestOperaciones(ConanFile):
     name = "operaciones"
-    version = "0.1.1"
+    version = "0.1.2"
     settings = "os", "compiler", "build_type", "arch"
     generators = "make"
     exports_sources = "src/*"
-    reference = "operaciones/0.1.1@uxpos/testing"
+    reference = "operaciones/0.1.2@uxpos/testing"
     license = "Sodimac UXPOS 1.0"
     url = "https://github.com/0zc4r1n/operaciones"
     description = "Libreria para operaciones basicas matematicas"
     options = {"shared": [True, False]}
     default_options = {"shared": False}
-    requires = "operaciones/0.1.1@uxpos/stable"
+    requires = "operaciones/0.1.2@uxpos/stable"
 
     def build(self):
         with tools.chdir("src"):
